349f0921a97cd166dcc6ffc45094efa10b27c5ae
zhukun <zkun325@gmail.com>
2025-05-13 20:47:58 +0800

    Initial Commit

---
+++ Klotski/KlotskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlotskiApp.swift
+//  Klotski
+//
+//  Created by zhukun on 2025/5/13.
+//
+
+import SwiftUI
+
+@main
+struct KlotskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KlotskiTests/KlotskiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KlotskiTests.swift
+//  KlotskiTests
+//
+//  Created by zhukun on 2025/5/13.
+//
+
+import Testing
+@testable import Klotski
+
+struct KlotskiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
